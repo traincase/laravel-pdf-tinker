6a832a5ffc5d2f13aa85a164139bc9f75499fae4
Jelle Roorda <jelle@beeldvoerders.nl>
2020-05-11 22:13:57 +0200

    Do not open browser while running dusk tests.

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -9,6 +9,13 @@ class BrowserTestCase extends TestCase
     protected static $baseServeHost = '127.0.0.1';
     protected static $baseServePort = 4444;
 
+    protected function setUp(): void
+    {
+        \Orchestra\Testbench\Dusk\Options::withoutUI();
+
+        parent::setUp();
+    }
+
     protected function getPackageProviders($app)
     {
         return ['Traincase\LaravelPdfTinker\PdfTinkerServiceProvider'];
